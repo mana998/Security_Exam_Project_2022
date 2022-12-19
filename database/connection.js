a27c1df13609a9dd1ded5eba24b1c1cce81bc869
Marianna <mana998@azet.sk>
2022-12-19 20:07:34 +0100

    Added draft role based connection

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,40 @@
 require('dotenv').config();
 
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host     : process.env.HOST,
-    database : process.env.DATABASE,
-    user     : process.env.USER,
-    password : process.env.PASSWORD
-})
 
-connection.connect();
+function getConnection(userType) {
+    const connectionParams = {
+        host: process.env.HOST,
+        database: process.env.DATANASE
+    }
+    switch(userType){
+        case "admin": {
+            connectionParams.user = process.env.ADMIN_USER
+            connectionParams.password = process.env.ADMIN_PASSWORD
+            break;
+        }
+        case "user": {
+            connectionParams.user = process.env.NORMAL_USER
+            connectionParams.password = process.env.NORMAL_PASSWORD
+
+            break;
+        }
+        default: {
+            connectionParams.user = process.env.GUEST_USER
+            connectionParams.password = process.env.GUEST_PASSWORD
+            break;
+        }
+    }
+    const connection = mysql.createConnection(connectionParams);
+    connection.connect();
+    return connection;
+}
+
+function disconnect(connection) {
+    connection.disconnect();
+}
 
 module.exports = {
-    connection
+    getConnection,
+    disconnect
 }
